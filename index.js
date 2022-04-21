d63e23e67108b7001e736222c9e221af1a1256a0
Jesus Hernandez <hdmjesus@gmail.com>
2022-04-20 23:26:43 -0400

    port edit

--- index.js
+++ index.js
@@ -225,7 +225,13 @@ async function startApolloServer (typeDefinitions, resolvers) {
   await server.start()
   server.applyMiddleware({ app })
 
-  app.listen(3000)
+  app.listen(process.env.PORT || 3000, function () {
+    console.log(
+      'Express server listening on port %d in %s mode',
+      this.address().port,
+      app.settings.env
+    )
+  })
 }
 
 startApolloServer(typeDefinitions, resolvers)
